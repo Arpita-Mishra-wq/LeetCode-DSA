405a2b6910c8dddf03b5a6c4774ededf3de18325
Arpita Mishra <arpita13351@gmail.com>
2026-03-13 21:28:57 +0530

    11

---
+++ Single_Number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        if len(nums) == 1:
+            return nums[0]
+        nums=sorted(nums)
+        for i in range(0,len(nums)-1,2):
+            if nums[i]!=nums[i+1]:
+                return nums[i]
+        return nums[-1]
