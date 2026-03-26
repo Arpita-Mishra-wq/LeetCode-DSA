e38071a5bfbfebda094fdba2d259d279793cb97f
Arpita Mishra <arpita13351@gmail.com>
2026-03-27 00:06:20 +0530

    57

---
+++ Convert_date_to_binary.py
@@ -0,0 +1,7 @@
+class Solution:
+    def convertDateToBinary(self, date: str) -> str:
+        date=date.split("-")
+        for i in range(len(date)):
+            date[i]=bin(int(date[i]))[2::]
+        date="-".join(date)
+        return date
