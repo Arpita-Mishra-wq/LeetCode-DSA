2c5ecfb2d39b9060bc50c1d6e40a1064f37ac931
Arpita Mishra <arpita13351@gmail.com>
2026-03-27 01:05:08 +0530

    78

---
+++ Teemo_Attacking.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
+        answer=duration
+        if len(timeSeries)==1:
+            return duration
+        for i in range(len(timeSeries)-1):
+            if (timeSeries[i+1]-timeSeries[i])>=duration:
+                answer+=duration
+            else:
+                answer+=timeSeries[i+1]-timeSeries[i]
+        return answer
